ccd8456177ecf271337be4154105b475686160ec
Myakubusani <myakubusani@gmail.com>
2026-07-04 20:47:31 +0100

    Initial FarmLink project

---
+++ js/dashboard.js
@@ -0,0 +1,15 @@
+function goSell() {
+    window.location.href = "sell.html";
+}
+
+function goBuy() {
+    window.location.href = "buyers.html";
+}
+
+function goRatings() {
+    window.location.href = "ratings.html";
+}
+
+function goProfile() {
+    window.location.href = "profile.html";
+}
